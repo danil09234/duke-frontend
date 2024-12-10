76e01d7390b634125186e6fda720fbed63ebe3ae
danil09234 <dailzag05@gmail.com>
2024-12-10 15:00:16 +0100

    Add chat title

--- duke-chat/components/MyAssistant.tsx
+++ duke-chat/components/MyAssistant.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import {AssistantRuntimeProvider, useEdgeRuntime} from "@assistant-ui/react";
+import { AssistantRuntimeProvider, useEdgeRuntime } from "@assistant-ui/react";
 import { MyThread } from "@/components/ui/assistant-ui/thread";
 import { makeMarkdownText } from "@assistant-ui/react-markdown";
+import TopBarWrapper from "@/components/ui/TopBarWrapper";
 
 const MarkdownText = makeMarkdownText();
 
@@ -10,12 +11,13 @@ export function MyAssistant() {
   const runtime = useEdgeRuntime({ api: "/api/chat" });
 
   return (
-    <div className="p-0 flex-1">
-        <AssistantRuntimeProvider runtime={runtime}>
-          <MyThread
-            assistantMessage={{ components: { Text: MarkdownText } }}
-          />
-        </AssistantRuntimeProvider>
+    <div className="flex flex-col p-0 flex-1 h-screen">
+      <TopBarWrapper />
+      <AssistantRuntimeProvider runtime={runtime} className="flex-1">
+        <MyThread
+          assistantMessage={{ components: { Text: MarkdownText } }}
+        />
+      </AssistantRuntimeProvider>
     </div>
   );
 }
