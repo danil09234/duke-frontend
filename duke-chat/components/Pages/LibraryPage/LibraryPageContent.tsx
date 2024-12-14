d6ab7805a83d07ba2b808f3ccafd47ce99bc0e26
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2024-12-14 18:44:32 +0100

    Update Topbar on LibraryPage

--- duke-chat/components/Pages/LibraryPage/LibraryPageContent.tsx
+++ duke-chat/components/Pages/LibraryPage/LibraryPageContent.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import TopBarWrapperChats from "@/components/Topbar/TopBarWrapperChats";
 import LibraryContent from "@/components/Pages/LibraryPage/LibraryContent";
+import TopBarWrapperLibrary from "@/components/Topbar/TopBarWrapperLibrary";
 
 export function LibraryPageContent() {
   return (
     <div className="flex flex-col py-0 h-full overflow-auto relative bg-white">
-      <TopBarWrapperChats />
+      <TopBarWrapperLibrary />
       <LibraryContent />
     </div>
   );
