31afd8fd2b76730959daaa54f2e2ca50e463508d
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2024-12-14 19:08:48 +0100

    Add tabs

--- duke-chat/components/Pages/LibraryPage/LibraryContent.tsx
+++ duke-chat/components/Pages/LibraryPage/LibraryContent.tsx
@@ -1,18 +1,97 @@
 import React from "react";
-import { SearchIcon, ListFilter, ChevronDown } from "lucide-react";
+import {
+  SearchIcon,
+  ListFilter,
+  ChevronDown,
+  MessagesSquare,
+  Heart,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import LibraryGrid from "@/components/Pages/LibraryPage/LibraryGrid";
-import { WelcomeCard } from "../../PromptBox/WelcomeCard";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const LibraryContent = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center px-4 py-10 sm:px-6 md:px-8 lg:px-[100] overflow-y-auto scrollbar-hide">
       <div className="flex flex-col gap-10 max-w-4xl w-full">
-        <WelcomeCard
-          userName="Danylo"
-          welcomeMessage="Vitaj na stránke asistenta pre uchádzačov!"
-          inputPlaceholder="Ako vám môžem pomôcť?"
-        />
+        <Tabs defaultValue="account" className="w-[400px] ">
+          <TabsList className="grid h-10 w-full grid-cols-2 bg-[#F0F2F5]">
+            <TabsTrigger
+              value="account"
+              className="h-full  data-[state=active]:text-[#19213D] data-[state=inactive]:text-[#666F8D]"
+            >
+              <MessagesSquare className="w-4 h-4 mr-2" />
+              Všetky chaty
+            </TabsTrigger>
+            <TabsTrigger
+              value="password"
+              className="h-full data-[state=active]:text-[#19213D] data-[state=inactive]:text-[#666F8D]"
+            >
+              <Heart className="w-4 h-4 mr-2" />
+              Obľúbené
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="account">
+            <Card>
+              <CardHeader>
+                <CardTitle>Account</CardTitle>
+                <CardDescription>
+                  Make changes to your account here. Click save when you're
+                  done.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" defaultValue="Pedro Duarte" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="username">Username</Label>
+                  <Input id="username" defaultValue="@peduarte" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save changes</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+
+          <TabsContent value="password">
+            <Card>
+              <CardHeader>
+                <CardTitle>Password</CardTitle>
+                <CardDescription>
+                  Change your password here. After saving, you'll be logged out.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="current">Current password</Label>
+                  <Input id="current" type="password" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="new">New password</Label>
+                  <Input id="new" type="password" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save password</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
 
         <div className="h-full flex flex-col gap-8 w-full overflow-y-hidden overflow-x-visible">
           <div className="flex flex-col items-left sm:flex-row sm:items-center justify-between w-full overflow-x-visible">
