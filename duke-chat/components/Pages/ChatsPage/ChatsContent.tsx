f285888ff8e6add87c4a0ab67cbb8d41bf1450f8
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2024-12-15 01:44:34 +0100

    Add gradient to the background

--- duke-chat/components/Pages/ChatsPage/ChatsContent.tsx
+++ duke-chat/components/Pages/ChatsPage/ChatsContent.tsx
@@ -6,7 +6,15 @@ import { WelcomeCard } from "@/components/PromptBox/WelcomeCard";
 
 const ChatsContent = (): JSX.Element => {
   return (
-    <div className="flex flex-col items-center px-4 py-10 sm:px-6 md:px-8 lg:px-[100] overflow-y-auto scrollbar-hide">
+    <div
+      className="flex flex-col items-center px-4 py-10 sm:px-6 md:px-8 lg:px-[100] overflow-y-auto scrollbar-hide"
+      style={{
+        backgroundImage: "url('/resources/big-background-gradient.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "0% 0%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div className="flex flex-col gap-10 max-w-4xl w-full">
         <WelcomeCard
           userName="Vladyslav"
@@ -24,7 +32,7 @@ const ChatsContent = (): JSX.Element => {
              w-full sm:w-auto overflow-visible sm:flex-nowrap"
             >
               <div className="flex items-center rounded-md shadow-sm border border-gray-200 bg-white w-full sm:w-auto h-10 px-3 py-1.5">
-                <SearchIcon className="w-5 h-5 text-[#666F8D] mr-2" />
+                <SearchIcon className="w-4 text-[#666F8D] mr-2" />
                 <input
                   type="text"
                   className="flex-grow placeholder-[#666F8D] bg-transparent focus:outline-none"
--- duke-chat/components/Pages/LibraryPage/LibraryPageContent.tsx
+++ duke-chat/components/Pages/LibraryPage/LibraryPageContent.tsx
@@ -5,7 +5,15 @@ import TopBarWrapperLibrary from "@/components/Topbar/TopBarWrapperLibrary";
 
 export function LibraryPageContent() {
   return (
-    <div className="flex flex-col py-0 h-full overflow-auto relative bg-white">
+    <div
+      className="flex flex-col py-0 h-full overflow-auto relative bg-white"
+      style={{
+        backgroundImage: "url('/resources/big-background-gradient.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "0% 0%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <TopBarWrapperLibrary />
       <LibraryContent />
     </div>
