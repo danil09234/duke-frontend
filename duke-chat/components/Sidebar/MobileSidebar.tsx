2ea15e5ddff95e6cf03e9800508d0a8c96a3846e
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2024-12-16 13:09:00 +0100

    Refactor sidebar mobile version

--- duke-chat/components/Sidebar/MobileSidebar.tsx
+++ duke-chat/components/Sidebar/MobileSidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { usePathname } from "next/navigation";
 import {
@@ -10,9 +9,7 @@ import {
   LucideMessageCircle,
   Puzzle,
   Plus,
-  MessageCircle,
   Search,
-  PuzzleIcon,
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -22,15 +19,22 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-  DrawerClose,
 } from "@/components/ui/drawer";
 import { TooltipIconButton } from "../ui/assistant-ui/tooltip-icon-button";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import styles from "@/styles/SidebarWrapper.module.css";
 
 const sections = [
   { name: "Chaty", href: "/chats", icon: LucideMessageCircle },
   { name: "Knižnica", href: "/library", icon: Book },
-  { name: "Nájsť program", href: "/chats/chat123", icon: Puzzle },
+  { name: "Nájsť program", href: "/chats/chat1234", icon: Puzzle },
 ];
 
 export function MobileSidebar() {
@@ -39,92 +43,125 @@ export function MobileSidebar() {
 
   return (
     <Drawer direction="left" open={open} onOpenChange={setOpen}>
-      <nav className="flex overflow-hidden flex-wrap gap-10 justify-between items-center px-4 py-4 max-w-screen-md bg-white border-b border-gray-200 max-md:px-4">
-        <DrawerTrigger asChild>
-          <Button variant="outline" size="icon" className="md:hidden">
-            <Menu className="h-5 w-5" />
-          </Button>
-        </DrawerTrigger>
-        <Avatar className="col-start-1 row-span-full row-start-1">
-          <AvatarImage
-            src="/resources/duke-avatar.png"
-            alt="Assistant Avatar"
-          />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-        <TooltipIconButton
-          tooltip="New Chat"
-          side="right"
-          className="bg-[#FF4100] w-9 h-9 rounded-[8px] hover:bg-[#FF4100]/90"
-        >
-          <Plus color="white" />
-        </TooltipIconButton>
-      </nav>
-
-      <DrawerContent className="w-[296px] p-0 pb-4 pt-[2px] bg-slate-50">
-        <div className="flex flex-col h-full">
-          <div className="p-4">
-            <DrawerHeader className="p-0">
-              <div className="flex justify-between items-center">
-                <div className="rounded-md">
-                  <Button
-                    onClick={() => setOpen(false)}
-                    variant="outline"
-                    size="icon"
-                    className="md:hidden"
-                  >
-                    <VisuallyHidden.Root>
-                      <DrawerTitle>Close Sidebar</DrawerTitle>
-                    </VisuallyHidden.Root>
-                    <Menu className="h-5 w-5" />
-                  </Button>
-                </div>
-                <div className="flex gap-0 text-[#666F8D]">
-                  <Button variant="ghost" size="icon">
-                    <Search className="h-5 w-5" />
-                  </Button>
-                  <Button variant="ghost" size="icon">
-                    <Plus className="h-5 w-5" />
-                  </Button>
-                </div>
-              </div>
-            </DrawerHeader>
-
-            <nav className="mt-10 space-y-1">
-              <div
-                className="flex items-center px-3 py-2.5
-               text-sm font-medium rounded-lg bg-white text-slate-800 shadow-sm"
+      <NavigationMenu>
+        <NavigationMenuList className="flex overflow-hidden flex-wrap gap-10 justify-between items-center px-4 py-4 max-w-screen-md bg-white border-b border-gray-200 max-md:px-4">
+          <DrawerTrigger asChild>
+            <Button variant="outline" size="icon" className="md:hidden">
+              <Menu className="h-5 w-5" />
+            </Button>
+          </DrawerTrigger>
+          <Avatar className="col-start-1 row-span-full row-start-1">
+            <AvatarImage
+              src="/resources/duke-avatar.png"
+              alt="Assistant Avatar"
+            />
+            <AvatarFallback>A</AvatarFallback>
+          </Avatar>
+          <NavigationMenuItem>
+            <Link to={"/chats/chat1234/"}>
+              <TooltipIconButton
+                tooltip="New Chat"
+                side="right"
+                className="bg-[#FF4100] w-9 h-9 rounded-[8px] hover:bg-[#FF4100]/90"
               >
-                <MessageCircle className="mr-2 h-4 w-4" />
-                <span className="flex-1">Chaty</span>
+                <Plus color="white" />
+              </TooltipIconButton>
+            </Link>
+          </NavigationMenuItem>
+
+          <DrawerContent className="w-[296px] p-0 pb-4 pt-[2px] bg-slate-50">
+            <div className="flex flex-col h-full">
+              <div className="p-4">
+                <DrawerHeader className="p-0">
+                  <div className="flex justify-between items-center">
+                    <div className="rounded-md">
+                      <Button
+                        onClick={() => setOpen(false)}
+                        variant="outline"
+                        size="icon"
+                        className="md:hidden"
+                      >
+                        <VisuallyHidden.Root>
+                          <DrawerTitle>Close Sidebar</DrawerTitle>
+                        </VisuallyHidden.Root>
+                        <Menu className="h-5 w-5" />
+                      </Button>
+                    </div>
+                    <NavigationMenuList className="flex gap-0 text-[#666F8D]">
+                      <Button variant="ghost" size="icon">
+                        <Search className="h-5 w-5" />
+                      </Button>
+                      <NavigationMenuItem>
+                        <Link
+                          to={"/chats/chat1234/"}
+                          onClick={() => setOpen(false)}
+                        >
+                          <Button variant="ghost" size="icon">
+                            <Plus className="h-5 w-5" />
+                          </Button>
+                        </Link>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </div>
+                </DrawerHeader>
+
+                <NavigationMenuList className="mt-10 space-y-1">
+                  {sections.map((section, index) => {
+                    const isActive = pathname === section.href;
+                    return (
+                      <NavigationMenuItem
+                        key={index}
+                        className={`${
+                          styles.defaultSidebarItem
+                        } h-fit hover:bg-[#e7ebf0] ${
+                          isActive ? styles.activeSidebarItem : ""
+                        }`}
+                        asChild
+                      >
+                        <Link
+                          to={section.href}
+                          className={`flex items-center justify-between w-full group rounded-md transition-colors gap-2 px-3 py-3 ${
+                            isActive
+                              ? `${styles.activeSidebarItem} pointer-events-none`
+                              : `${styles.defaultSidebarItem} hover:bg-[#e7ebf0] active:bg-[#edf0f4]`
+                          }`}
+                          onClick={() => setOpen(false)}
+                        >
+                          <section.icon
+                            className={`${
+                              isActive ? "text-[#19213D]" : "text-[#666F8D]"
+                            } h-5 w-5`}
+                          />
+                          <span
+                            className={`${
+                              isActive ? "text-[#19213D]" : "text-[#666F8D]"
+                            } flex-grow`}
+                          >
+                            {section.name}
+                          </span>
+                        </Link>
+                      </NavigationMenuItem>
+                    );
+                  })}
+                </NavigationMenuList>
               </div>
 
-              <div className="flex items-center px-3 py-2.5 text-sm font-medium rounded-lg text-slate-500 hover:bg-[#e7ebf0]">
-                <Book className="mr-2 h-4 w-4" />
-                <span className="flex-1">Knižnica</span>
+              <div className="mt-auto px-4 border-">
+                <div className="flex items-center w-full">
+                  <img
+                    src="/resources/av2024-small.jpg"
+                    alt="User avatar"
+                    className="w-8 h-8 rounded-full shadow-sm object-cover"
+                  />
+                  <span className="ml-2 text-sm font-medium text-slate-800">
+                    Vladyslav P.
+                  </span>
+                </div>
               </div>
-
-              <div className="flex items-center px-3 py-2.5 text-sm font-medium rounded-lg text-slate-500 hover:bg-[#e7ebf0]">
-                <PuzzleIcon className="mr-2 h-4 w-4" />
-                <span className="flex-1">Nájsť program</span>
-              </div>
-            </nav>
-          </div>
-
-          <div className="mt-auto px-4 border-">
-            <div className="flex items-center w-full">
-              <img
-                src="/resources/av2024-small.jpg"
-                alt="User avatar"
-                className="w-8 h-8 rounded-full shadow-sm object-cover"
-              />
-              <span className="ml-2 text-sm font-medium text-slate-800">
-                Vladyslav P.
-              </span>
             </div>
-          </div>
-        </div>
-      </DrawerContent>
+          </DrawerContent>
+        </NavigationMenuList>
+      </NavigationMenu>
     </Drawer>
   );
 }
