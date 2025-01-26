fe64077df43a787d5867a393ccfe7e38fc237bbe
danil09234 <dailzag05@gmail.com>
2025-01-26 02:10:31 +0200

    Update colors

--- duke-chat/components/FinalStudyProgramme/FinalStudyProgramme.tsx
+++ duke-chat/components/FinalStudyProgramme/FinalStudyProgramme.tsx
@@ -28,10 +28,10 @@ const FinalStudyProgramme = ({ programmeName }: FinalStudyProgrammeProps): JSX.E
                         variant="link"
                         className="flex items-center justify-center gap-1 w-full text-components-links-text-text-accent hover:no-underline"
                     >
-                        <span className="font-display-2-medium text-[14px] leading-[130%] font-medium">
+                        <span className="font-display-2-medium text-[14px] leading-[130%] font-medium text-components-buttons-primary-text-text-dark-mode">
                             Čítať viac o štúdiu: {programmeName}
                         </span>
-                        <ArrowRight className="w-4 h-4" />
+                        <ArrowRight className="w-4 h-4 text-components-buttons-primary-text-text-dark-mode" />
                     </Button>
                 </div>
             </CardContent>
