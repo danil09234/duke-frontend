8d11d86bc5d6bcce7f71dc429a2770783ce43817
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2025-02-13 01:45:23 +0100

    remove temporary input in welcome card

--- duke-chat/components/PromptBox/WelcomeCard.tsx
+++ duke-chat/components/PromptBox/WelcomeCard.tsx
@@ -16,7 +16,7 @@ export function WelcomeCard({
 }: WelcomeCardProps) {
   return (
     <div
-      className="flex w-full overflow-vidible flex-col justify-center items-center px-8 py-12 bg-white rounded-lg border border-gray-200 border-solid shadow-sm max-md:px-5 gap-6"
+      className="flex w-full overflow-vidible flex-col justify-center items-center px-8 py-12 bg-white rounded-lg border border-gray-200 border-solid shadow-sm max-md:px-5 gap-4"
       style={{
         backgroundImage: "url('/resources/background-gradient.svg')",
         backgroundSize: "cover",
@@ -26,7 +26,7 @@ export function WelcomeCard({
       <BadgeDisclaimer />
       <div className="flex flex-col max-w-[400px] w-full">
         <UserGreeting userName={userName} welcomeMessage={welcomeMessage} />
-        <AssistantInput placeholder={inputPlaceholder} />
+        {/* <AssistantInput placeholder={inputPlaceholder} /> */}
       </div>
     </div>
   );
