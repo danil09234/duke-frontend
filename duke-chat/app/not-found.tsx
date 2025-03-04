dfd66da6ebb31bb9de7738416241acc7369e8db8
Vladyslav Panik <vladyslav.panik@student.tuke.sk>
2025-03-05 00:30:13 +0100

    Enhance 404 page with Slovak localization and improved layout

--- duke-chat/app/not-found.tsx
+++ duke-chat/app/not-found.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Custom404() {
-  return <h1>404 - - Page Not Found</h1>;
+  return (
+    <div className="min-h-screen flex justify-center">
+      <div className="mt-[20vh] text-center flex flex-col gap-[32px] w-full px-5">
+        <div className="self-center text-[32px] text-[#19213D] font-bold leading-tight max-w-[660px] w-full">
+          Whoops táto stránka nie je k dispozícii 😵‍💫
+        </div>
+        <Link href="/">
+          <Button className="inline-flex items-center gap-1 bg-[#FF4100] hover:bg-[#FF4100]/90 text-white border-none font-display-1-medium text-xs font-medium rounded-[8px]">
+            <span>Prejsť na hlavnú</span>
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
